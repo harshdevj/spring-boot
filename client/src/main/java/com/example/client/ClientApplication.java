16fee8466fb31214e60ea70b67b67c79ea1c9a77
Mohak <Mohak@mohak>
2018-08-30 23:35:08 +0530

    SSL

--- client/src/main/java/com/example/client/ClientApplication.java
+++ client/src/main/java/com/example/client/ClientApplication.java
@@ -10,7 +10,24 @@ import org.springframework.context.annotation.Configuration;
 @EnableAutoConfiguration
 public class ClientApplication {
 
+	static {
+		javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier(new javax.net.ssl.HostnameVerifier() {
+			public boolean verify(String hostname, javax.net.ssl.SSLSession sslSession) {
+				if (hostname.equals("localhost")) {
+					return true;
+				}
+				return false;
+			}
+		});
+
+		System.setProperty("javax.net.ssl.trustStore", "classpath:truststore.jks");
+		System.setProperty("javax.net.ssl.trustStorePassword", "keypassword");
+		//System.setProperty("javax.net.ssl.keyStore", "classpath:keystore.jks");
+		//System.setProperty("javax.net.ssl.keyStorePassword", "keypassword");
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(ClientApplication.class, args);
 	}
+
 }
